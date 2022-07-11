e0dbd064df2f44cf27b022602bd09d509e0d3bca
Ink Phoenix <99095585+InkPhoenix@users.noreply.github.com>
2022-07-11 04:04:52 +0100

    11-7-2022_0404

--- SRC/OSE/Assets/Scripts/Main Menu/TitleScreen/scr_main_title.cs
+++ SRC/OSE/Assets/Scripts/Main Menu/TitleScreen/scr_main_title.cs
@@ -8,9 +8,9 @@ public class scr_main_title : MonoBehaviour
 {
     public CanvasGroup UI_element;
     private float fade_time = 2.0f;
-    private Guid uid;
-    private Sequence sequence;
-    private bool sequence_stopped;
+    private Guid uid_title;
+    private Sequence sequence_title;
+    private int sequence_stopped;
     public AudioSource musicSource;
 
     private IEnumerator fadeFromBlack()
@@ -33,31 +33,71 @@ public class scr_main_title : MonoBehaviour
 
     private void Start()
     {
-        sequence_stopped = false;
+        sequence_stopped = 0;
         UI_element.alpha = 0;
         if (scr_fade_levelLoader.make_fade_black_on_startup == true) { StartCoroutine(fadeFromBlack()); }
         StartCoroutine(startupFade());
     }
 
-    public void startFade() { UI_element.DOFade(1, fade_time); }
+    public void startFade()
+    {
+        if (sequence_title == null) //create if there's none before
+        {
+            sequence_title = DOTween.Sequence();
+            sequence_title.Append(UI_element.DOFade(1, fade_time).OnComplete(setValue));
+            uid_title = System.Guid.NewGuid();
+            sequence_title.id = uid_title;
+        }
+        sequence_title.Play();
+    }
+
+    private void setValue() { sequence_stopped = 3; }
 
     public void stopFade()
     {
-        if (sequence_stopped == false) //do this to only activate once
+        switch (sequence_stopped)
         {
-            sequence_stopped = true;
-            UI_element.alpha = 1;
+            case 0: //code to skip animation
+                sequence_stopped++;
 
-            scn_switch(); //start scene switching fade
+                //kill fade
+                scr_fade_levelLoader loader_script = GameObject.Find("Crossfade").GetComponent<scr_fade_levelLoader>();
+                CanvasGroup loader_script_alpha = GameObject.Find("Crossfade").GetComponent<CanvasGroup>();
+                loader_script.killSequence();
+                loader_script_alpha.alpha = 0;
 
-            //animate title fade-out
-            Vector3 title_original_scale = transform.localScale;
-            Vector3 title_scale_to = title_original_scale * 1.8f; //scale up the title by 1.8
+                //kill title anim
+                DOTween.Kill(uid_title);
+                sequence_title = null;
+                UI_element.alpha = 1;
+            break;
+
+            case 1:
+                sequence_stopped++;
+            break;
+
+            case 2:
+                sequence_stopped++;
+            break;
+
+            case 3: //code to change scenes
+                sequence_stopped++;
+                UI_element.alpha = 1;
+
+                scn_switch(); //start scene switching fade
+
+                //animate title fade-out
+                Vector3 title_original_scale = transform.localScale;
+                Vector3 title_scale_to = title_original_scale * 1.8f; //scale up the title by 1.8
             
-            UI_element.transform.DOScale(title_scale_to, 2.0f);
-            UI_element.DOFade(0, 2.0f);
+                UI_element.transform.DOScale(title_scale_to, 2.0f);
+                UI_element.DOFade(0, 2.0f);
 
-            DOTween.To(() => musicSource.volume, x => musicSource.volume = x, 0, 2.0f).SetEase(Ease.InSine); //fade-out BGM volume
+                DOTween.To(() => musicSource.volume, x => musicSource.volume = x, 0, 2.0f).SetEase(Ease.InSine); //fade-out BGM volume
+            break;
+
+            default:
+            break;
         }
     }
 
--- SRC/OSE/Assets/Scripts/Main Menu/TitleScreen/scr_start_text.cs
+++ SRC/OSE/Assets/Scripts/Main Menu/TitleScreen/scr_start_text.cs
@@ -18,18 +18,21 @@ public class scr_start_text : MonoBehaviour
     private float fade_time = 0.8f;
     private Guid uid;
     private Sequence sequence;
-    private bool sequence_stopped;
+    private int sequence_stopped;
+    public IEnumerator crout_id;
 
-    private void Start()
+    public void Start()
     {
-        sequence_stopped = false;
+        sequence_stopped = 0;
         UI_element.alpha = 0;
-        StartCoroutine(startupTextFade());
+        crout_id = startupTextFade();
+        StartCoroutine(crout_id);
     }
 
     private IEnumerator startupTextFade()
     {
         yield return new WaitForSeconds(3.5f); //wait before fading-in the text
+        sequence_stopped = 3;
         startTextFade();
     }
 
@@ -47,40 +50,59 @@ public class scr_start_text : MonoBehaviour
 
     public void stopFade()
     {
-        //kill the sequence with ID uid and set sequence to NULL
-        DOTween.Kill(uid);
-        sequence = null;
-
-        //only execute this once
-        if (sequence_stopped == false)
+        switch (sequence_stopped)
         {
-            //set alphas to 1
-            sequence_stopped = true;
-            UI_element.alpha = 1;
-            back_fade.alpha = 1;
-            front_fade.alpha = 1;
+            case 0: //code to skip animation
+                sequence_stopped++;
+                UI_element.alpha = 0;
+                StopCoroutine(crout_id);
+                startTextFade();
+            break;
 
-            //play SFX
-            SFXSource.Play();
+            case 1:
+                sequence_stopped++;
+                break;
 
-            //start animations
-            Vector3 text_original_scale = transform.localScale;
-            float text_scale_to = text_original_scale.x * 1.35f; //scale up the text by 1.35
+            case 2:
+                sequence_stopped++;
+                break;
 
-            //Tween the text spacing
-            DOTween.To(() => UI_element_text.characterSpacing, x => UI_element_text.characterSpacing = x, 1.5f, 0.75f);
-            DOTween.To(() => back_fade_text.characterSpacing, x => back_fade_text.characterSpacing = x, 1.5f, 0.75f);
-            DOTween.To(() => front_fade_text.characterSpacing, x => front_fade_text.characterSpacing = x, 1.5f, 0.75f);
+            case 3: //code to change scenes
+                //kill the sequence with ID uid and set sequence to NULL
+                DOTween.Kill(uid);
+                sequence = null;
 
-            //Twwen the text scale
-            UI_element.transform.DOScaleX(text_scale_to, 0.75f);
-            back_fade.transform.DOScaleX(text_scale_to, 0.75f);
-            front_fade.transform.DOScaleX(text_scale_to, 0.75f);
+                //set alphas to 1
+                sequence_stopped++;
+                UI_element.alpha = 1;
+                back_fade.alpha = 1;
+                front_fade.alpha = 1;
 
-            //Tween the text fade
-            UI_element.DOFade(0, 0.75f);
-            back_fade.DOFade(0, 0.25f);
-            front_fade.DOFade(0, 0.25f);
+                //play SFX
+                SFXSource.Play();
+
+                //start animations
+                Vector3 text_original_scale = transform.localScale;
+                float text_scale_to = text_original_scale.x * 1.35f; //scale up the text by 1.35
+
+                //Tween the text spacing
+                DOTween.To(() => UI_element_text.characterSpacing, x => UI_element_text.characterSpacing = x, 1.5f, 0.75f);
+                DOTween.To(() => back_fade_text.characterSpacing, x => back_fade_text.characterSpacing = x, 1.5f, 0.75f);
+                DOTween.To(() => front_fade_text.characterSpacing, x => front_fade_text.characterSpacing = x, 1.5f, 0.75f);
+
+                //Tween the text scale
+                UI_element.transform.DOScaleX(text_scale_to, 0.75f);
+                back_fade.transform.DOScaleX(text_scale_to, 0.75f);
+                front_fade.transform.DOScaleX(text_scale_to, 0.75f);
+
+                //Tween the text fade
+                UI_element.DOFade(0, 0.75f);
+                back_fade.DOFade(0, 0.25f);
+                front_fade.DOFade(0, 0.25f);
+            break;
+
+            default:
+            break;
         }
     }
 }
